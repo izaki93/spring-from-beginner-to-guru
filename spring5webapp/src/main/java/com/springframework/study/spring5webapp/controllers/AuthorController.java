0dca5a0b247dd178908ecc64dcc41067e8f8b8cd
Ibrahim Mohsen <ibrahim.mohsen.zaki@gmail.com>
2019-12-12 20:14:40 +0200

    Add AuthorContorller and Authors.html with thymeleaf

---
+++ spring5webapp/src/main/java/com/springframework/study/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,22 @@
+package com.springframework.study.spring5webapp.controllers;
+
+import com.springframework.study.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+        model.addAttribute("authors",authorRepository.findAll());
+        return "authors";
+    }
+}
