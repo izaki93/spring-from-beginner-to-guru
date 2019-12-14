98ee595a0c17f329ba73e9e56942556d23941fb3
Ibrahim Mohsen <ibrahim.mohsen.zaki@gmail.com>
2019-12-14 20:36:37 +0200

    manual DI with property ,setter and consturctor Injection

---
+++ di-demo/src/main/java/com/springframework/study/DiDemoApplication.java
@@ -0,0 +1,18 @@
+package com.springframework.study;
+
+import com.springframework.study.controllers.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+
+        MyController myController = (MyController) ctx.getBean("myController");
+
+        myController.hello();
+    }
+}
